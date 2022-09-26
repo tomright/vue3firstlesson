738938606dd61ad8bad05da8177a25ad7e72d064
TomRight <zxvc@rambler.ru>
2022-09-27 00:29:58 +0500

    Добавил полифил для того чтобы не ругался на новый вебпак и остановился на input

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,87 @@
 <template>
-  <div>
-    Количество лайков: <strong>{{ likes }}</strong>
+  <div class="app">
+    <form class="form">
+      <h4>Создание поста</h4>
+      <input
+        v-bind:value="title"
+        @input="inputTitle"
+        class="input"
+        type="text"
+        placeholder="Название"
+      />
+      <input
+        v-bind:value="body"
+        @input="inputBody"
+        class="input"
+        type="text"
+        placeholder="Описание поста"
+      />
+      <button class="btn">Добавить пост</button>
+    </form>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название:</strong> {{ post.title }}</div>
+      <div><strong>Описание:</strong> {{ post.body }}</div>
+    </div>
   </div>
-  <div>
-    Количество дислайков: <strong>{{ dislikes }}</strong>
-  </div>
-  <button @click="addLike">Лайк!</button>
-  <button @click="addDislike">Дизлайк!</button>
 </template>
 <script>
+import { title } from "process";
+
 export default {
   data() {
     return {
-      likes: 0,
-      dislikes: 0,
+      posts: [
+        { id: 1, title: "JavaScript", body: "Описание поста" },
+        { id: 2, title: "JavaScript 2", body: "Описание поста 2" },
+        { id: 3, title: "JavaScript 3", body: "Описание поста 3" },
+        { id: 4, title: "JavaScript 4", body: "Описание поста 4" },
+      ],
+      title: "",
+      body: "",
     };
   },
   methods: {
-    addLike() {
-      this.likes += 1;
+    inputTitle(event) {
+      this.title = event.target.value;
     },
-    addDislike() {
-      this.dislikes += 1;
-    },
+    inputBody(event) {
+      this.body = event.target.value;
+}
   },
 };
 </script>
-<style></style>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 20px;
+}
+.post {
+  padding: 15px;
+  margin-top: 15px;
+  border: 3px solid teal;
+}
+.input {
+  width: 100%;
+  padding: 10px, 15px;
+  border: 1px solid teal;
+  margin-top: 15px;
+  height: 35px;
+}
+.form {
+  display: flex;
+  flex-direction: column;
+}
+.btn {
+  align-self: flex-end;
+  margin-top: 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+  height: 30px;
+  width: 120px;
+}
+</style>
